0eccabfcd06cb59e6ae755de3845226095e952a8
Júlia Sales Novais <142419446+julsales@users.noreply.github.com>
2024-09-02 00:07:44 -0300

    Alguma mudanças de legibilidade

--- CodigoFonte/main.c
+++ CodigoFonte/main.c
@@ -1,213 +1,1 @@
-// Calculadora didática
-// Autora: Júlia Sales, Periodo : 3B
-// Primeira questão: 24/08
-// Segunda questão: 30/08
-// Terceira questão:
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-typedef struct Node {
-  int num;
-  struct Node *next;
-} Node;
-
-void imprimir_elementos(Node *head) {
-  while (head != NULL) {
-    if (head->num >= 10 && head->num <= 15) {
-      printf("%c", 'A' + (head->num - 10));
-    } else {
-      printf("%d", head->num);
-    }
-    head = head->next;
-  }
-}
-
-void guardar_elementos(Node **head, int res) {
-  Node *novo = (Node*)malloc(sizeof(Node));
-  if (novo != NULL) {
-    novo->num = res;
-    novo->next = *head;
-    *head = novo;
-  }
-}
-
-void calcular_base2(int *n) {
-  Node *head = NULL;
-  if(*n<0){
-      *n= *n * -1;
-  }
-  printf("\nConversão do número decimal %d para base 2:", *n);
-  while (1) {
-    int res = *n % 2;
-    if (*n <= 1) {
-      printf("\n %d/2 não tem resto inteiro, então é %d", *n, *n);
-    } else {
-      printf("\n %d/2 tem resto: %d", *n, res);
-    }
-    *n = *n / 2;
-    guardar_elementos(&head, res);
-    if (*n < 1) {
-      printf("\n O total é: ");
-      imprimir_elementos(head);
-      break;
-    }
-  }
-}
-
-void calcular_base8(int *n) {
-  Node *head = NULL;
-  if(*n<0){
-      *n= *n * -1;
-  }
-  printf("\nConversão do número decimal %d para base 8:", *n);
-  while (1) {
-    int res = *n % 8;
-    if (*n <= 1) {
-      printf("\n %d/8 não tem resto inteiro, então é %d", *n, *n);
-    } else {
-      printf("\n %d/8 tem resto: %d", *n, res);
-    }
-    *n = *n / 8;
-    guardar_elementos(&head, res);
-    if (*n < 1) {
-      printf("\n O total é: ");
-      imprimir_elementos(head);
-      break;
-    }
-  }
-}
-
-void calcular_base16(int *n) {
-  Node *head = NULL;
-  if(*n<0){
-      *n= *n * -1;
-  }
-  printf("\nConversão do número decimal %d para base 16:", *n);
-  while (1) {
-    int res = *n % 16;
-    if (*n <= 1) {
-      printf("\n %d/16 não tem resto inteiro, então é %d", *n, *n);
-    } else {
-      printf("\n %d/16 tem resto: %d", *n, res);
-    }
-    *n = *n / 16;
-    guardar_elementos(&head, res);
-    if (*n < 1) {
-      printf("\n O total é: ");
-      imprimir_elementos(head);
-      break;
-    }
-  }
-}
-
-void calcular_BCD(int *n) {
-  Node *head = NULL;
-  if(*n<0){
-      *n= *n * -1;
-  }
-  printf("\nConversão do número decimal %d para BCD:", *n);
-  int cont=1;
-  while (*n>0) {
-    int digito = *n%10;
-    printf("\nPasso %d: Convertendo o dígito %d para BCD (4 bits): ",cont, digito);
-    for (int i = 0; i < 4; i++) {
-      int bit = (digito >> i) & 1;
-      guardar_elementos(&head, bit);
-    }
-    imprimir_elementos(head);
-    *n=*n/10;
-    cont++;
-  }
-  printf("\n O total é: ");
-  imprimir_elementos(head);
-}
-
-void calcular_complemento_a2(int *n) {
-    Node *head1 = NULL;
-    Node *head2 = NULL;
-    int bits[16] = {0};
-    int negativo = 0;
-    printf("\n Representação de %d em complemento de 2 com 16 bits: ", *n);
-    if (*n < 0) {
-        *n = *n * -1;
-        negativo = 1;
-    }printf("\n Transforma o número em binário com 16 bits: ");
-    for (int i = 0; i < 16; i++) {
-        bits[15 - i] = *n % 2;
-        *n /= 2;
-    }
-    for (int i = 15; i >= 0; i--) {
-        guardar_elementos(&head1, bits[i]);
-    }
-    imprimir_elementos(head1);
-
-    if (negativo == 1) {
-        printf("\n Agora inverte todos o bits: ");
-        for (int i = 0; i < 16; i++) {
-            printf("\n %d vira: ",bits[i]);
-            bits[i] = !bits[i];
-            printf("%d ",bits[i]);
-        }printf("\n E somando +1 aos bits invertidos o resultado é: ");
-        for (int i = 15; i >= 0; i--) {
-            if (bits[i] == 0) {
-                bits[i] = 1;
-                break;
-            } else {
-                bits[i] = 0;
-            }
-        }
-        for (int i = 15; i >= 0; i--) {
-            guardar_elementos(&head2, bits[i]);
-        }
-        imprimir_elementos(head2);
-        printf("\n");
-    }
-}
-
-int main(void) {
-  int n;
-  int escolha;
-  do{
-      printf("\n Escolha um processo"
-             "\n 1- Calcular base 2"
-             "\n 2- Calcular base 8"
-             "\n 3- Calcular base 16"
-             "\n 4- Calcular BCD"
-             "\n 5- Transformar base 10 para base com sinal com 16 bits (Complemento A2) "
-             "\n 6- Converter real em decimal para float e double"
-             "\n 7- Sair\n");
-      scanf("\n %d", &escolha);
-      if (escolha!=7){
-          printf("\n Escolha um número para calcular: ");
-          scanf("%d", &n);
-      }
-      switch (escolha) {
-          case 1:
-              calcular_base2(&n);
-              break;
-          case 2:
-              calcular_base8(&n);
-              break;
-          case 3:
-              calcular_base16(&n);
-              break;
-          case 4:
-              calcular_BCD(&n);
-              break;
-          case 5:
-              calcular_complemento_a2(&n);
-              break;
-          case 6:
-              break;
-          case 7:
-              break;
-          default:
-              printf("\n Opção inválida");
-              break;
-      }
-  }while(escolha!=7);
-
-    return 0;
-}
+/*** *     _____       _            _           _                   ______ _     _       _   _ *    /  __ \     | |          | |         | |                  |  _  (_)   | |     | | (_) *    | /  \/ __ _| | ___ _   _| | __ _  __| | ___  _ __ __ _   | | | |_  __| | __ _| |_ _  ___ __ _ *    | |    / _` | |/ __| | | | |/ _` |/ _` |/ _ \| '__/ _` |  | | | | |/ _` |/ _` | __| |/ __/ _` | *    | \__/\ (_| | | (__| |_| | | (_| | (_| | (_) | | | (_| |  | |/ /| | (_| | (_| | |_| | (_| (_| | *     \____/\__,_|_|\___|\__,_|_|\__,_|\__,_|\___/|_|  \__,_|  |___/ |_|\__,_|\__,_|\__|_|\___\__,_| * * */// Autora: Júlia Sales, Periodo : 3B// Primeira questão: 24/08// Segunda questão: 30/08// Terceira questão:#include <stdio.h>#include <stdlib.h>#include "unistd.h"typedef struct Node {  int num;  struct Node *next;} Node;void imprimir_elementos(Node *head);void guardar_elementos(Node **head, int res);void calcular_base(int *n,int base);void calcular_BCD(int *n);void calcular_complemento_a2(int *n);void float_double(double n);int main(void) {  int n;  int escolha;  printf("\t\n------- Calculadora Didática --------\n");  do{      sleep(1);      printf("\n Escolha um processo:\n"             "\n 1- Calcular base 2"             "\n 2- Calcular base 8"             "\n 3- Calcular base 16"             "\n 4- Calcular BCD"             "\n 5- Transformar base 10 para base com sinal com 16 bits (Complemento A2) "             "\n 6- Converter real em decimal para float e double"             "\n 7- Sair \n");      scanf("\n %d", &escolha);      if (escolha>0 && escolha<6){          printf("\n Escolha um número para calcular: ");          scanf("%d", &n);      }      switch (escolha) {          case 1:              calcular_base(&n,2);              break;          case 2:              calcular_base(&n,8);              break;          case 3:              calcular_base(&n,16);              break;          case 4:              calcular_BCD(&n);              break;          case 5:              calcular_complemento_a2(&n);              break;          case 6:              float n2;              printf("\n Escolha um número para calcular: ");              scanf("%f", &n2);              float_double(n2);              break;          case 7:              break;          default:              printf("\n Opção inválida");              break;      }  }while(escolha!=7);    return 0;}void imprimir_elementos(Node *head) {    while (head != NULL) {        if (head->num >= 10 && head->num <= 15) {            printf("%c", 'A' + (head->num - 10));        } else {            printf("%d", head->num);        }        head = head->next;    }}void guardar_elementos(Node **head, int res) {    Node *novo = (Node*)malloc(sizeof(Node));    if (novo != NULL) {        novo->num = res;        novo->next = *head;        *head = novo;    }}void calcular_base(int *n, int base) {    Node *head = NULL;    if(*n<0){        *n= *n * -1;    }    printf("\nConversão do número decimal %d para base %d:", *n, base);    while (1) {        int res = *n % base;        if (*n <= 1) {            printf("\n %d/%d não tem resto inteiro, então é %d", *n, base, *n);            sleep(1);        } else {            printf("\n %d/%d tem resto: %d", *n, base, res);\            sleep(1);        }        *n = *n/base;        guardar_elementos(&head, res);        if (*n < 1) {            printf("\n Invertendo todos os dígitos...");            sleep(1);            printf("\n O total é: ");            imprimir_elementos(head);            printf("\n");            sleep(1);            break;        }    }}void calcular_BCD(int *n) {    Node *head = NULL;    if(*n<0){        *n= *n * -1;    }    printf("\nConversão do número decimal %d para BCD:", *n);    sleep(1);    int cont=1;    while (*n>0) {        int digito = *n%10;        printf("\nPasso %d: Convertendo o dígito %d para BCD (4 bits): ",cont, digito);        sleep(1);        for (int i=0; i<4; i++) {            int bit = (digito >> i) & 1;            guardar_elementos(&head, bit);        }        imprimir_elementos(head);        *n=*n/10;        cont++;    }    sleep(1);    printf("\n O total é: ");    imprimir_elementos(head);    printf("\n");}void calcular_complemento_a2(int *n) {    Node *head1 = NULL;    Node *head2 = NULL;    int bits[16] = {0};    int negativo = 0;    printf("\n Representação de %d em complemento de 2 com 16 bits: ", *n);    sleep(1);    if (*n < 0) {        *n = *n * -1;        negativo = 1;    }printf("\n Transforma o número em binário com 16 bits: ");    sleep(1);    for (int i = 0; i<16; i++) {        bits[15 - i] = *n % 2;        *n /= 2;    }    for (int i = 15; i >= 0; i--) {        guardar_elementos(&head1, bits[i]);    }    imprimir_elementos(head1);    if (negativo == 1) {        printf("\n Agora inverte todos o bits: ");        for (int i = 0; i < 16; i++) {            printf("\n %d vira: ",bits[i]);            bits[i] = !bits[i];            printf("%d ",bits[i]);            sleep(1);        }printf("\n E somando +1 aos bits invertidos o resultado é: ");        for (int i = 15; i >= 0; i--) {            if (bits[i] == 0) {                bits[i] = 1;                break;            } else {                bits[i] = 0;            }        }        for (int i = 15; i>=0; i--) {            guardar_elementos(&head2, bits[i]);        }        sleep(1);        imprimir_elementos(head2);        printf("\n");    }}void float_double(double n) {}
