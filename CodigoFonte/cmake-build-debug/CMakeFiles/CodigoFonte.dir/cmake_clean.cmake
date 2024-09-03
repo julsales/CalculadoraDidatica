94c0f03908809c05f99bc91fe9964ad01cb7e3f6
Júlia Sales Novais <jsn@cesar.school>
2024-09-02 22:05:21 -0300

    finalizado
    fiz a questão 3 kkk

---
+++ CodigoFonte/cmake-build-debug/CMakeFiles/CodigoFonte.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodigoFonte.dir/main.c.obj"
+  "CMakeFiles/CodigoFonte.dir/main.c.obj.d"
+  "CodigoFonte.exe"
+  "CodigoFonte.exe.manifest"
+  "CodigoFonte.pdb"
+  "libCodigoFonte.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CodigoFonte.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
